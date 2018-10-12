96028633a2bdd8cd04b73239da334825d1bccf6c
github username <github email address>
2018-10-12 13:40:40 -0400

    seeds changed

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :artist
+  attributes :id, :name, :artist, :email
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-u1 = User.create(name: 'pete', artist: false)
+u1 = User.create(name: 'pete', artist: false, email: 'peter@email.com')
 
-a1 = Artwork.create(nickname: 'graffiti1', user: u1, latitude: 100, longitude: 100, img_url: 'https://www.amsterdammuseum.nl/sites/default/files/styles/image/public/teasers/rhyme.jpg?itok=TDXbuSBT&timestamp=1441097523')
+a1 = Artwork.create(nickname: 'graffiti1', user: u1, latitude: 40.780059, longitude: -73.951443, img_url: 'https://www.amsterdammuseum.nl/sites/default/files/styles/image/public/teasers/rhyme.jpg?itok=TDXbuSBT&timestamp=1441097523')
+a2 = Artwork.create(nickname: 'graffiti2', user: u1, latitude: 40.790100, longitude: -73.941411, img_url: 'https://imgc.allpostersimages.com/img/print/posters/sammyc-graffiti-tag-thats-red_a-G-10361177-14258384.jpg')
+a3 = Artwork.create(nickname: 'graffiti3', user: u1, latitude: 40.770200, longitude: -73.931521, img_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/2009_010_CES_utka.JPG/1200px-2009_010_CES_utka.JPG')
