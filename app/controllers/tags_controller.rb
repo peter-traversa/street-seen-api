c13dc383db5a0005b9f9860fd6a18eaad49eb30f
github username <github email address>
2018-10-17 16:15:29 -0400

    tags serializer fixed

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,11 @@
 class TagsController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  # GET /tags
+  def index
+    @tags = Tag.all
+
+    render json: @tags
+  end
+
 end
--- app/serializers/tag_serializer.rb
+++ app/serializers/tag_serializer.rb
@@ -1,3 +1,3 @@
 class TagSerializer < ActiveModel::Serializer
-  attributes :id, :type
+  attributes :id, :name
 end
