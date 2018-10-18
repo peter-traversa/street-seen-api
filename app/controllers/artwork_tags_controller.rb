a6e3713e21ceb6c6547f89c7296a93a8ebe13805
github username <github email address>
2018-10-18 18:06:57 -0400

    small changes to artwork tag serializer

--- app/controllers/artwork_tags_controller.rb
+++ app/controllers/artwork_tags_controller.rb
@@ -1,4 +1,30 @@
 class ArtworkTagsController < ApplicationController
-  belongs_to :artworks
-  belongs_to :tags
+  before_action :set_tag, only: [:show]
+
+  def index
+    @artwork_tags = ArtworkTag.all
+
+    render json: @artwork_tags
+  end
+
+  def create
+    @artwork_tag = ArtworkTag.new(artwork_tag_params)
+
+    if @artwork_tag.save
+      render json: @artwork_tag, status: :created, location: @artwork_tag
+    else
+      render json: @artwork_tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def set_tag
+      @artwork_tag = ArtworkTag.find(params[:id])
+    end
+
+    def artwork_tag_params
+      params.permit(:tag_id, :artwork_id)
+    end
+
 end
--- app/serializers/artwork_tag_serializer.rb
+++ app/serializers/artwork_tag_serializer.rb
@@ -1,3 +1,5 @@
 class ArtworkTagSerializer < ActiveModel::Serializer
   attributes :id, :artwork_id, :tag_id
+  belongs_to :artwork
+  belongs_to :tag
 end
