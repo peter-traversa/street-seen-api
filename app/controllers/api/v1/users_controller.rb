07659a43b89edf40a100b2dd969f4d9f3c80a68e
github username <github email address>
2018-11-15 20:52:58 -0500

    ready to merge to master

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::UsersController < ApplicationController
   def profile
     render json: { user: UserSerializer.new(current_user) }, status: :accepted
   end
+
+  def index
+    @users = User.all
+  end
  
   def create
     @user = User.create(user_params)
